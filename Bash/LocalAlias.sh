f35a871f6ef85df0e8f2c1987ad503a4412aa18d
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2019-04-06 19:01:28 +0900

    設定の変更
    (1) エイリアスを追加

---
+++ Bash/LocalAlias.sh
@@ -0,0 +1,16 @@
+
+##
+##  基本的なエイリアスの設定。
+##
+##  @file   Bash/LocalAlias.sh
+##
+##  Copyright (C) 2014/07,  Takahiro Itou.
+##  All Rights Reserved.
+##
+
+alias  screen='screen -T xterm-256color'
+
+if [ "x${MYBASH_ECHO_LOADING_BASHRC}y" = "xTRUEy" ] ; then
+  echo  "Loading  ${HOME}/LocalSettings/Bash/LocalAlias.sh : OK"  1>&2
+fi
+
